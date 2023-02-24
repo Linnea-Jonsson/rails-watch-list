48c1d67efbf862fb33f8b082080c3a1a302366d7
Linnea Jonsson <linneafjonsson@gmail.com>
2023-02-24 14:56:58 +0000

    form updated

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,5 @@
 class BookmarksController < ApplicationController
   def new
-    @bookmark = Bookmark.new
     @list = List.find(params[:list_id])
   end
 
@@ -13,6 +12,7 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
     @bookmark.destroy
     redirect_to list_path(@list), status: :see_other
   end
